bc70c50bcd30f63ed5bb9ab0943bef3e5cee87ea
Jirka <del@hs-furtwangen.de>
2018-04-10 12:48:00 +0200

    L02_DOM upload

--- L02_DOM/DOM.js
+++ L02_DOM/DOM.js
@@ -1,5 +1,17 @@
 var L02;
 (function (L02) {
-    console.log("Hallo");
+    window.addEventListener("load", init);
+    function init(_event) {
+        console.log("Hallo");
+        let h = document.createElement("h1");
+        h.innerText = "Dies ist eine generierte Überschrift";
+        console.log(document);
+        document.body.appendChild(h);
+        let fieldsets = document.getElementsByTagName("fieldset");
+        for (let i = 0; i < fieldsets.length; i++) {
+            console.log(fieldsets[i]);
+            console.log(fieldsets[i].textContent);
+        }
+    }
 })(L02 || (L02 = {}));
 //# sourceMappingURL=DOM.js.map
--- L02_DOM/DOM.ts
+++ L02_DOM/DOM.ts
@@ -1,3 +1,17 @@
 namespace L02 {
-    console.log("Hallo");
+    window.addEventListener("load", init);
+
+    function init(_event: Event): void {
+        console.log("Hallo");
+        let h: HTMLHeadingElement = document.createElement("h1");
+        h.innerText = "Dies ist eine generierte Überschrift";
+        console.log(document);
+        document.body.appendChild(h);
+
+        let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
+        for (let i: number = 0; i < fieldsets.length; i++) {
+            console.log(fieldsets[i]);
+            console.log(fieldsets[i].textContent);
+        }
+    }
 }
